9070ba389b75dbfc8a75929b47acb2df26025ee4
dang duc <dangtrungduc89@gmail.com>
2018-09-30 19:53:05 +0700

    Add push function

--- ThreadPool.h
+++ ThreadPool.h
@@ -14,41 +14,59 @@
 
 #include "noncopyable.h"
 #include "Callbacks.h"
+#include <future>
 
 namespace ev
 {
 
-class ThreadPool: noncopyable
-{
-public:
-    explicit
-    ThreadPool(size_t numThread,
-               size_t maxQueueSize = 65536,
-               const ThreadInitCallback& cb = nullptr);
-    ~ThreadPool();
+  class ThreadPool: noncopyable
+  {
+    public:
+      explicit
+        ThreadPool(size_t numThread,
+            size_t maxQueueSize = 65536,
+            const ThreadInitCallback& cb = nullptr);
+      ~ThreadPool();
+      void runTask(Task&& task);
+      void stop();
+      size_t numThreads() const
+      { return threads_.size(); }
 
-    void runTask(const Task& task);
-    void runTask(Task&& task);
-    void stop();
-    size_t numThreads() const
-    { return threads_.size(); }
+      template <class F, class... Args>
 
-private:
-    void runInThread(size_t index);
-    Task take();
+        auto push(F&& function, Args&&... args)
+        -> std::future<typename std::result_of<F(Args...)>::type> {
+          using ReturnType = typename std::result_of<F(Args...)>::type;
 
-    typedef std::unique_ptr<std::thread> ThreadPtr;
-    typedef std::vector<ThreadPtr> ThreadList;
+          auto task = std::make_shared<std::packaged_task<ReturnType()>>(
+              std::bind(std::forward<F>(function), std::forward<Args>(args)...));
+          auto future = task->get_future();
 
-    ThreadList threads_;
-    std::mutex mutex_;
-    std::condition_variable notEmpty_;
-    std::condition_variable notFull_;
-    std::deque<Task> taskQueue_;
-    const size_t maxQueueSize_;
-    std::atomic_bool running_;
-    ThreadInitCallback threadInitCallback_;
-};
+          auto functor = [this, task = std::move(task)]() mutable {
+            (*task)();
+          };
+
+          this->runTask(std::move(functor));
+
+          return future;
+        }
+
+    private:
+      void runInThread(size_t index);
+      Task take();
+
+      typedef std::unique_ptr<std::thread> ThreadPtr;
+      typedef std::vector<ThreadPtr> ThreadList;
+
+      ThreadList threads_;
+      std::mutex mutex_;
+      std::condition_variable notEmpty_;
+      std::condition_variable notFull_;
+      std::deque<Task> taskQueue_;
+      const size_t maxQueueSize_;
+      std::atomic_bool running_;
+      ThreadInitCallback threadInitCallback_;
+  };
 
 }
 
